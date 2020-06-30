39be3bf8156304b787762e612198e134b1b90aa8
Otavio Capila <otaviocapila@gmail.com>
2020-06-30 16:48:13 -0300

    created file to export all environment variables

---
+++ src/config/environment.ts
@@ -0,0 +1,9 @@
+import assertExists from 'ts-assert-exists';
+
+export default {
+  CONSUMER_KEY: assertExists(process.env.CONSUMER_KEY),
+  CONSUMER_SECRET: assertExists(process.env.CONSUMER_SECRET),
+  ACCESS_TOKEN: assertExists(process.env.ACCESS_TOKEN),
+  ACCESS_TOKEN_SECRET: assertExists(process.env.ACCESS_TOKEN_SECRET),
+  BOT_ID: assertExists(process.env.BOT_ID),
+};
