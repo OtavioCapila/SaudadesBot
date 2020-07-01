2bd91c32f96c610e4e5d913e6e493071230d8a21
Otavio Capila <otaviocapila@gmail.com>
2020-07-01 10:29:13 -0300

    removed express

--- build/index.js
+++ build/index.js
@@ -1,15 +1,5 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 require("./streams/limited");
 require("./streams/unlimited");
-var logger_1 = __importDefault(require("./logger"));
-var express_1 = __importDefault(require("express"));
-var app = express_1.default();
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-    logger_1.default.log("- SERVER - Escutando na porta " + port + " ");
-});
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 import 'dotenv/config';
 import './streams/limited';
 import './streams/unlimited';
-import logger from './logger';
-import express from 'express';
-
-const app = express();
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  logger.log(`- SERVER - Escutando na porta ${port} `);
-});
