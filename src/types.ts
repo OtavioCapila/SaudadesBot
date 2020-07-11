23bc6be7c85cf7228ba67111b5e60c0f7cabc400
Otavio Capila <otaviocapila@gmail.com>
2020-07-10 22:20:12 -0300

    added a note related to the custom type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,9 @@
 import { Twitter } from 'twit';
 
+/**
+ * Since twit library is outdated, i had to create this custom type;
+ * I've opened an PR to fix that https://github.com/DefinitelyTyped/DefinitelyTyped/pull/45854
+ */
 export type UpdatedTwitterStatus = Twitter.Status & {
   is_quote_status: boolean;
 };
