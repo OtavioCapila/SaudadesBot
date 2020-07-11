54b5c232495c335a074103da9f1483a71f24ff95
Otavio Capila <otaviocapila@gmail.com>
2020-07-10 22:20:37 -0300

    added type coercion and a message if the variable is not defined

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,10 +1,10 @@
 import assertExists from 'ts-assert-exists';
 
 export default {
-  CONSUMER_KEY: assertExists(process.env.CONSUMER_KEY),
-  CONSUMER_SECRET: assertExists(process.env.CONSUMER_SECRET),
-  ACCESS_TOKEN: assertExists(process.env.ACCESS_TOKEN),
-  ACCESS_TOKEN_SECRET: assertExists(process.env.ACCESS_TOKEN_SECRET),
-  BOT_ID: assertExists(process.env.BOT_ID),
-  DELAY: assertExists(process.env.DELAY),
+  CONSUMER_KEY: assertExists(process.env.CONSUMER_KEY,'Consumer key not provided'),
+  CONSUMER_SECRET: assertExists(process.env.CONSUMER_SECRET,'Consumer secret not provided'),
+  ACCESS_TOKEN: assertExists(process.env.ACCESS_TOKEN,'Access token not provided'),
+  ACCESS_TOKEN_SECRET: assertExists(process.env.ACCESS_TOKEN_SECRET,'Access token secret not provided'),
+  BOT_ID: assertExists(process.env.BOT_ID,'Bot id not provided'),
+  DELAY: Number(assertExists(process.env.DELAY,'Delay not provided')),
 };
