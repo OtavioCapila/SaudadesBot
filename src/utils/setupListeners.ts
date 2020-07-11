894e7800314abaf4b3aaf4d6d379d9604e0227ce
Otavio Capila <otaviocapila@gmail.com>
2020-07-10 22:19:43 -0300

    created a function to setup all listeners

---
+++ src/utils/setupListeners.ts
@@ -0,0 +1,23 @@
+import { Stream } from "twit";
+import logger from "../logger";
+
+const setupListeners = (stream: Stream, type: "UNLIMITED" | "LIMITED") => {
+  stream.on("disconnect", (disconnectMessage: string) => {
+    logger.error(`- ${type} - Desconectado - ${disconnectMessage}`);
+    stream.start();
+  });
+
+  stream.on("connect", () => {
+    logger.log(`- ${type} - Conectando`);
+  });
+
+  stream.on("connected", () => {
+    logger.log(`- ${type} - Conectado`);
+  });
+
+  stream.on("warning", (warning: string) => {
+    logger.warn(`- ${type} - ${warning}`);
+  });
+};
+
+export default setupListeners;
